3e2024ec49247090f4e4ab34fd3de8b5d22f2667
mattbtindall <matthewbtindall@gmail.com>
2023-04-02 11:28:22 +0100

    Explore entity methods on repository

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Entity\Movie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class mainController extends AbstractController
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     #[Route('/', name: 'app_homepage')]
     public function homepage(): Response
     {
-        return $this->render('app/homepage.html.twig', [
-            'title' => 'homepage'
-        ]);
+        // findAll() - SELECT * FROM mvoies;
+        // find() - SELECT * FROM movies WHERE id = 5;
+        // findBy([], ['id' => 'DESC']) - SELECT * FROM movies ORDER BY id DESC - the empty array means get all the entities
+        // findOneBy(['id' => 5, 'title' => 'The Dark Knight'], ['id' => 'DESC']) - SELECT * FROM movies WHERE id = 5 AND title = 'The Dark Knight' ORDER BY id DESC
+        // count(['id' => 5]) - SELECT COUNT() WHERE id = 5
+
+        $repository = $this->em->getRepository(Movie::class);
+
+        $movies = $repository->getClassName();
+        dd($movies);
+        return $this->render('app/homepage.html.twig');
     }
 }
