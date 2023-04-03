646430d3534fee38fa419d808c7df623e30f7fc1
mattBtindall <matthewbtindall@gmail.com>
2023-04-03 11:08:01 +0100

    Prepare to start project

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\MovieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    private $movieRepository;
+
+    public function __construct(MovieRepository $movieRepository)
+    {
+        $this->movieRepository = $movieRepository;
+    }
+
+    #[Route('/movies', name: 'movies')]
+    public function index(): Response
+    {
+        $movies = $this->movieRepository->findAll();
+        dd($movies);
+
+        return $this->render('movies/test.html.twig', [
+            'phpinfo' => phpinfo()
+        ]);
+    }
+}
