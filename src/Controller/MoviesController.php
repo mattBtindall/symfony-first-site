77577b9b7682c08f84b5710ac1f771c6736edd24
mattbtindall <matthewbtindall@gmail.com>
2023-04-06 08:39:15 +0100

    Add delete movie

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -116,6 +116,16 @@ class MoviesController extends AbstractController
         ]);
     }
 
+    #[Route('/movies/delete/{id}', methods: ['GET', 'DELETE'], name: 'delete')]
+    public function delete($id): Response
+    {
+        $movie = $this->movieRepository->find($id);
+        $this->em->remove($movie);
+        $this->em->flush();
+
+        return $this->redirectToRoute('movies');
+    }
+
     #[Route('/movies/{id}', methods: ['GET'], name: 'show')]
     public function show($id): Response
     {
